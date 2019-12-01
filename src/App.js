5a1983f79560f796a607bd92683854fc56fd83dc
Vincedream <callmevincehua@gmail.com>
2019-12-01 23:09:28 +0800

    feat: add some decorator

--- src/App.js
+++ src/App.js
@@ -1,27 +1,89 @@
 import React from 'react';
 import './App.css';
 
-function sayAge(target, name, descriptor) {
-  let sayName = descriptor.value;
+function toast(message) {
+  alert(message)
+}
+
+function setUserName(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.5) {
+        resolve({data: `userName is ${name}`})
+      } else {
+        reject(new Error('接口错误啦~'))
+      }
+    }, 200)
+  })
+}
+
+// function sayAge(target, name, descriptor) {
+//   let sayName = descriptor.value;
+//   descriptor.value = function() {
+//     sayName.apply(this);
+//     console.log('age: 12');
+//   }
+// }
+
+// class Cat {
+//   name = 'vince'
+//   @sayAge
+//   sayName() {
+//     console.log(this.name)
+//   }
+// }
+
+function operateToast(target, key, descriptor) {
+  const originFunc = descriptor.value;
+
   descriptor.value = function() {
-    sayName.apply(this);
-    console.log('age: 12');
+    return originFunc.apply(this, arguments).then(res => {
+      toast('操作成功')
+      return res
+    }).catch(err => {
+      toast('操作失败')
+      return err
+    })
+  }
+  return descriptor
+}
+
+class UserApi {
+  @operateToast
+  static setUserName(name) {
+    return setUserName(name)
   }
 }
 
-class Cat {
-  name = 'vince'
-  @sayAge
-  sayName() {
-    console.log(this.name)
+function updateable(isUpdateable) {
+  return function(target) {
+    target.isUpdateable = isUpdateable;
+  }
+}
+
+function testDecorator(target, name, descriptor) {
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+@updateable(true)
+class DesignerApi {
+  @testDecorator
+  getUserDate() {
+    console.log('xxx')
   }
 }
 
 class App extends React.Component {
   componentDidMount() {
-    let catA = new Cat();
-    catA.sayName();
+    // let catA = new Cat();
+    // catA.sayName();
+    // UserApi.setUserName('vince')
+    // console.log(UserApi.xxx)
+    // console.log(DesignerApi.isUpdateable)
   }
+
   render() {
     return (
       <div>es7 装饰器</div>
